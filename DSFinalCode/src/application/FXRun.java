1f6cee53a82ecb908a1d01cd42c04d059dcbcf0a
Kyle <71513019+sterlingkatwit@users.noreply.github.com>
2021-11-30 19:44:15 -0500

    Add files via upload
    new file with javafx working

---
+++ DSFinalCode/src/application/FXRun.java
@@ -0,0 +1,49 @@
+package application;
+	
+		
+	import javafx.application.Application;
+	import javafx.event.ActionEvent;
+	import javafx.event.EventHandler;
+	import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+	import javafx.scene.control.Button;
+	import javafx.scene.layout.Pane;
+	import javafx.scene.layout.StackPane;
+	import javafx.stage.Stage;
+	import javafx.event.ActionEvent;
+	import javafx.fxml.FXML;
+	import javafx.scene.control.Button;
+	import javafx.scene.input.MouseEvent;
+	import javafx.scene.shape.Rectangle;
+	 
+	public class FXRun extends Application {
+	    
+	    
+	    @Override
+	   public void start(Stage primaryStage) throws Exception{
+	    	
+	    	try
+	    	{
+	    	Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("application/dsfinal.fxml"));
+	    	
+	    	Scene scene = new Scene(root);
+	    	primaryStage.setScene(scene);
+	    	primaryStage.show();
+	    	}
+	    	catch(Exception e)
+	    	{
+	    		e.printStackTrace();	
+	    	}
+	    	
+	    }
+	 
+	    public static void main(String[] args) {
+	        launch(args);
+	    }
+	}
+	
+	
+
+	
+	
